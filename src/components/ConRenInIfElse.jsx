591a083669a61460ef73d4f0f908db6d26a771f9
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2025-03-02 23:55:56 +0600

    conditional rendering in if else

--- src/components/ConRenInIfElse.jsx
+++ src/components/ConRenInIfElse.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+// Conditional rendering....
+
+// EX : 01
+// It is good for application,
+
+const LoginStatusBtn =(status)=>{//here we set our condition
+    if(status){
+        return <button>LogIn Btn</button>
+    }
+    else{
+        return <button>LogOut Btn</button>
+    }
+}
 
 const ConRenInIfElse = () => {
     return (
         <div>
-            conditional rendering
+            <h3>LogIn Button</h3>
+            {LoginStatusBtn(false)} 
         </div>
-    );
-};
+    )            
+    };
+    
+    export default ConRenInIfElse;
 
-export default ConRenInIfElse;
+
+
+
+
+// EX : 02 
+// It is not good for application, code is too long sometimes code is run slow
+
+// const ConRenInIfElse = () => {
+
+// const status = true;
+
+//     if(status == true){
+//         return (
+//             <div>
+//                 <h2>logIn button </h2>
+//                 <button>logIn</button>
+//             </div>
+//         );
+//     }
+//     else{
+//         return (
+//             <div>
+//                 <h2>logOut button </h2>
+//                 <button>logOut</button>
+//             </div>
+//         )
+//     }
+// };
+
+// export default ConRenInIfElse;
